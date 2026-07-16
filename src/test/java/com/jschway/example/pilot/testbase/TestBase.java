5ab08f8a82fcaeab76e94dc8e8b1e4f53cd01720
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-16 15:15:28 -0400

    Update report and use ITestResult testName when invoking createTest

--- src/test/java/com/jschway/example/pilot/testbase/TestBase.java
+++ src/test/java/com/jschway/example/pilot/testbase/TestBase.java
@@ -3,6 +3,7 @@ package com.jschway.example.pilot.testbase;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.jschway.example.pilot.ExtentReportManager;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,9 +12,9 @@ public class TestBase {
     public ExtentTest test;
     
     @BeforeMethod
-    public void init() { 
+    public void init(ITestResult res) { 
         report = ExtentReportManager.getReporter();
-        test = report.createTest("Test A");
+        test = report.createTest(res.getMethod().getMethodName().toUpperCase());
     }
     @AfterMethod
     public void quit() { 
