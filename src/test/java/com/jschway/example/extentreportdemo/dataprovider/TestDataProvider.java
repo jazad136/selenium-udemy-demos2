3a592647c9c0b47f610a3a242c5526e9fe6138d7
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-20 16:11:29 -0400

    Add @DataProvider

--- src/test/java/com/jschway/example/extentreportdemo/dataprovider/TestDataProvider.java
+++ src/test/java/com/jschway/example/extentreportdemo/dataprovider/TestDataProvider.java
@@ -1,10 +1,12 @@
 package com.jschway.example.extentreportdemo.dataprovider;
 
 import java.lang.reflect.Method;
+import org.testng.annotations.DataProvider;
 
 public class TestDataProvider {
-    
-    public void dataProviderSuiteA(Method method) { 
+
+    @DataProvider
+    public void dataProviderSuiteA(Method method) {
         // separate data for each test
         System.out.println("Test method name: " + method.getName());
     }
