4c6fcb921dc04f6a00be628107815e2c12eeff6e
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-07-22 08:13:31 -0400

    Use Attributes to Enable failure reporting

--- src/test/java/com/jschway/example/pilot/listener/CustomListener.java
+++ src/test/java/com/jschway/example/pilot/listener/CustomListener.java
@@ -4,6 +4,8 @@
  */
 package com.jschway.example.pilot.listener;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -15,11 +17,19 @@ public class CustomListener implements ITestListener {
  
     public void onTestFailure(ITestResult result) { 
         System.out.println("******** TEST FAILURE ********");
-        System.out.println("Test Name: " + result.getName());
-        System.out.println("Error : " + result.getThrowable().getMessage());
+//        System.out.println("Test Name: " + result.getTestName()); // can't use this
+//        System.out.println("Test Name: " + result.getName());
+//        System.out.println("Error : " + result.getThrowable().getMessage());
+//        System.out.println("Attribute Value : " + result.getAttribute("Author"));
+        ExtentTest test = (ExtentTest) result.getAttribute("reporterObject");
+        test.log(Status.INFO, "Test Case Name : " + result.getName());
+        test.fail(result.getThrowable().getMessage());
     }
        
     public void onTestSuccess(ITestResult result) { 
         System.out.println("******** TEST SUCCESS ********");
+        ExtentTest test = (ExtentTest) result.getAttribute("reporterObject");
+        test.log(Status.INFO, "Test Case Name : " + result.getName());
+        test.pass("Test Status is Success");
     }
 }
