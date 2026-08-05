319efdd6af5c41f685abe8b74fc6849d0a54cc33
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-08-05 16:34:38 -0400

    Really lean in to using testBase to ensure tests can be slowed down

---
+++ SeleniumDemoProject/src/test/java/com/techlynk/selenium/scenario2/SlowExTestBase.java
@@ -0,0 +1,32 @@
+package com.techlynk.selenium.scenario2;
+
+/**
+ *
+ * @author JonathanSaddler
+ */
+public abstract class SlowExTestBase extends ExTestBase {
+    final int slowMoSeconds;
+    public SlowExTestBase(String testPrepend) {
+        super(testPrepend);
+        this.testUrl = "https://jqueryui.com"; // default is jQueryUI.com
+        slowMoSeconds = 1;
+    }
+    public SlowExTestBase(String testPrepend, int slowMoSeconds) {
+        super(testPrepend);
+        this.testUrl = "https://jqueryui.com"; // default is jQueryUI.com
+        this.slowMoSeconds = slowMoSeconds;
+    }
+    
+    public void i(String msg) {
+        System.out.println(msg);
+        testReport.info(msg);
+        try {  Thread.sleep(slowMoSeconds*1000); } 
+        catch(InterruptedException e) {  throw new RuntimeException("Step execution interrupted."); }
+    }
+    public void iFormat(String formatMsg, Object... formatObjs) {
+        System.out.printf(formatMsg + "\n", formatObjs);
+        testReport.info(String.format(formatMsg + "\n", formatObjs));
+        try { Thread.sleep(slowMoSeconds*1000); } 
+        catch(InterruptedException e) { throw new RuntimeException("Step execution interrupted."); }
+    }
+}
