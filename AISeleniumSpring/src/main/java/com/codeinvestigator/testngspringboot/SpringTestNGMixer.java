6553851a3fdeb7cadb9f9a9d795164a6ecacb7f9
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-08-26 07:57:37 -0400

    [POM] Start AI Selenium Spring project

---
+++ AISeleniumSpring/src/main/java/com/codeinvestigator/testngspringboot/SpringTestNGMixer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.codeinvestigator.testngspringboot;
+
+/**
+ *
+ * @author JonathanSaddler
+ */
+public class SpringTestNGMixer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ AISeleniumSpring/src/main/java/com/codeinvestigator/testngspringboot/space/NavigationService.java
@@ -0,0 +1,11 @@
+package com.codeinvestigator.testngspringboot.space;
+
+/**
+ *
+ * @author JonathanSaddler
+ */
+public class NavigationService {
+    public String destination() { 
+        return "Mars";
+    }
+}
