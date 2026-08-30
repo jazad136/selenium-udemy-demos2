d2048703c9ed8fa6918240c32aaf91134635ef6e
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-08-29 21:04:18 -0400

    Update GenerateTestFromExternalizeStory to just write files

--- AISelenium/src/main/java/com/techlynk/selenium/GenerateTestFromExternalizeStory.java
+++ AISelenium/src/main/java/com/techlynk/selenium/GenerateTestFromExternalizeStory.java
@@ -1,6 +1,8 @@
 package com.techlynk.selenium;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class GenerateTestFromExternalizeStory {
 
@@ -10,7 +12,12 @@ public class GenerateTestFromExternalizeStory {
 		try {
 			String generatedCode = OpenAIClientWrapper.generateCodeFromStory(story);
 			System.out.println("\nAI Generated Test Code\n");
-			System.out.println(generatedCode);
+//			System.out.println(generatedCode);
+			// build timestamp-based filename and save
+			String ts = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+			String filename = "target/generated-tests/AI_GeneratedTest_" + ts + ".java";
+			OpenAIClientWrapper.saveToFile(generatedCode, filename);
+			System.out.println("\nSaved generated file: " + filename);
 		} catch(IOException e) { 
 			throw new RuntimeException("Could not generate code due to IOException", e);
 		}
