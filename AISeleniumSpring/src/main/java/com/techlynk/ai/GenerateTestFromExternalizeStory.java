4dc88e42414a78b40759e6a6a31811e0a29ae951
Jonathan Saddler <1129384+jazad136@users.noreply.github.com>
2026-09-17 10:51:32 -0400

    Add AISeleniumSpring folder to s11_practiceTestWeb

---
+++ AISeleniumSpring/src/main/java/com/techlynk/ai/GenerateTestFromExternalizeStory.java
@@ -0,0 +1,22 @@
+package com.techlynk.ai;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.techlynk.ai.config.UserStoryReader;
+
+@SpringBootApplication
+public class GenerateTestFromExternalizeStory implements CommandLineRunner {
+	
+	private UserStoryReader reader;
+	public GenerateTestFromExternalizeStory(UserStoryReader reader) { 
+		this.reader = reader;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String output = reader.readFromClasspath("user-story.txt");
+		System.out.println(output);
+	}
+}
+
